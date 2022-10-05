86b895616ca34dac18f656d98c4356c32b012983
RThemian <62709684+RThemian@users.noreply.github.com>
2022-10-04 21:25:55 -0600

    changed things around

--- frontend/src/views/CreateAccount.js
+++ frontend/src/views/CreateAccount.js
@@ -1,21 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Title } from "../layout/elements/Title";
 import { MainMenu } from "../layout/elements/MainMenu";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
 
-export const CreateAccount = ({
-    loggedInUser,
-}) => {
+export const CreateAccount = ({ loggedInUser }) => {
+  const createAccountContent = () => {
+    return <></>;
+  };
 
-    const createAccountContent = () => {
-        return (
-            <></>
-        );
-    }
-
-    return (
-        <>
-            <Title title="Create Account" />
-            <MainMenu content={createAccountContent()} />
-        </>
-    );
-}
+  return (
+    <>
+      <Title title="Create Account" />
+      <MainMenu content={createAccountContent()} />
+      <h1>Does this work?</h1>
+    </>
+  );
+};
